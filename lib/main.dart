5f06264477c9b1c6cba9abcbbe327b766bd6da5a
Poppy Sari <poppy.sari@rga.com>
2021-10-26 03:31:00 +0700

    :sparkle: feat: setup project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      BallPage(),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
       ),
     );
+  }
+}
